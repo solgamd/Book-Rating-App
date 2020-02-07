793fa9f43c1741c4a9f99856882273be3a7d81e6
solgamd <solgamd@gmail.com>
2020-02-06 18:20:09 -0600

    Table row content adding correctly; styling TBD

--- dom.js
+++ dom.js
@@ -5,15 +5,16 @@ $('.button').click(function() {
 $('.table-btn').insertAfter('#form');
 
 $('.table-btn').click(() => {
-    //insert input value into new place
     const name = $('.name-input').val();
     const rating = $('.rating-input').val();
 
-    let newName = $('td').text(name);
-    // let newRating = $('td').text(rating);
-    
-
-    $('table').append(newName);
+    let tr = $('<tr></tr>');
+    let td = $('<td>'+name+'</td><td>'+rating+'</td><td>'+'X'+'</td>')
+   
+    $('table').append(tr);
+    $(tr).addClass('table-rows')
+    $(tr).append(td);
+    // $(newName).append(`<td> + ${newRating} + </td>`);
     // $('table').append(newRating);
     $('.name-input').val('');
     $('.rating-input').val('');
