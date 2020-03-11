aeb4472eb747e96188efba34e72c788b0e0be5eb
solgamd <solgamd@gmail.com>
2020-03-10 19:51:22 -0500

    Updated table-btn click handler

--- dom.js
+++ dom.js
@@ -1,22 +1,24 @@
-$('.button').click(function() {
-    console.log('A button was clicked');
+$(".button").click(function() {
+  console.log("A button was clicked");
 });
 
-$('.table-btn').insertAfter('#form');
+$(".table-btn").insertAfter("#form");
 
-$('.table-btn').click(() => {
-    const name = $('.name-input').val();
-    const rating = $('.rating-input').val();
+$(".table-btn").click(() => {
+  const name = $(".name-input").val();
+  const rating = $(".rating-input").val();
 
-    let tr = $('<tr></tr>');
-    let td = $('<td>'+name+'</td><td>'+rating+'</td><td>'+'X'+'</td>')
-   
-    $('table').append(tr);
-    $(tr).addClass('table-rows')
-    $(tr).append(td);
-    // $(newName).append(`<td> + ${newRating} + </td>`);
-    // $('table').append(newRating);
-    $('.name-input').val('');
-    $('.rating-input').val('');
+  let tr = $("<tr></tr>");
+  let title = $("<td>" + name + "</td>");
+  let td = $('<td>'+name+'</td><td>'+rating+'</td><td>'+'X'+'</td>')
 
+  $("tbody").append(tr);
+  $(tr).addClass("table-rows");
+  $(tr).append(td);
+  
+
+  // $(newName).append(`<td> + ${newRating} + </td>`);
+  // $('table').append(newRating);
+  $(".name-input").val("");
+  $(".rating-input").val("");
 });
